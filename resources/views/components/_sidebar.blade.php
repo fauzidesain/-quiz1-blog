32834d517bc092f9d248646ccf340cc3431af60f
fauzidesain <86188082+fauzidesain@users.noreply.github.com>
2021-07-19 23:50:07 +0700

    kumpul tugas

---
+++ resources/views/components/_sidebar.blade.php
@@ -0,0 +1,23 @@
+<div class="sidebar">
+    <div class="app-title">KARIKATUR DIGITAL ONLINE</div>
+
+    <div class="sidebar-header">
+        <img src="{{ asset('images/profile-img.png') }}" alt="">
+        <div class="name-tag">
+            <div class="name">Anwar Fauzi</div>
+            <div class="status">Admin</div>
+        </div>
+    </div>
+
+    <div class="sidebar-menu-tag">Menu</div>
+    <div class="sidebar-content">
+        <ul class="sidebar-menu">
+            <li>
+                <a href="#" class="sidebar-menu-item">
+                    <i class="fas fa-desktop sidebar-icon-img"></i>
+                    <div class="sidebar-text-item">Menu Utama</div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</div>
